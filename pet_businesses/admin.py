6e9e2ad28fbf68a5d3d83b77e26fe3d199708db2
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-12-27 14:49:11 +0000

    Turn class name in singular

--- pet_businesses/admin.py
+++ pet_businesses/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Pet_Businesses
+from .models import Pet_Businesse, Service_Type, Pet_Type
 
 
 # Register your models here.
-admin.site.register(Pet_Businesses)
+admin.site.register(Pet_Businesse)
+admin.site.register(Service_Type)
+admin.site.register(Pet_Type)
--- pet_businesses/models.py
+++ pet_businesses/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
-# Create your models here.
-class Pet_Businesses (models.Model):
+class Pet_Businesse (models.Model):
     contact_first_name = models.CharField(max_length=50)
     contact_last_name = models.CharField(max_length=50)
-    contact_mobile = models.CharField(max_length=13)
+    contact_mobile = models.CharField(max_length=16)
     contact_email = models.EmailField()
     firm = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -14,19 +12,60 @@ class Pet_Businesses (models.Model):
     number = models.CharField(max_length=8)
     npa = models.CharField(max_length=13)
     locality = models.CharField(max_length=255)
-    phone = models.CharField(max_length=12)
+    phone = models.CharField(max_length=16)
     email = models.EmailField()
     website = models.URLField(blank=True, null=True)
     linkedin = models.URLField(blank=True, null=True)
     facebook = models.URLField(blank=True, null=True)
     instagram = models.URLField(blank=True, null=True)
     tiktok = models.URLField(blank=True, null=True)
-    #logo
-    #primary_image
-    #secondary_images
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
-    class Meta :
-        unique_together = ('firm', 'contact_first_name','contact_last_name')
+    def __str__(self):
+        return f"{self.firm}"
+
+
+class Service_Type (models.Model):
+    pet_business = models.ForeignKey(
+        Pet_Businesse, 
+        on_delete=models.SET_NULL,  
+        related_name="business_service_types",
+        null=True
+    )
+    service_type = models.CharField(max_length=150)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.service_type} ({self.pet_business})"
+
+
+class Pet_Type (models.Model):
+    pet_business = models.ForeignKey(
+        Pet_Businesse, 
+        on_delete=models.SET_NULL,  
+        related_name="business_pet_types",
+        null=True
+    )
+    pet_type = models.CharField(max_length=150)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.pet_type} ({self.pet_business})"
+
+
+class Pet_Type (models.Model):
+     pet_business = models.ForeignKey(
+         Pet_Businesse,  
+         on_delete=models.SET_NULL,  
+         related_name="business_pet_types",
+         null=True)
+     pet_type = models.CharField(max_length=150)
+     date_created = models.DateTimeField(auto_now_add=True)
+     last_modified = models.DateTimeField(auto_now=True)
+
+     def __str__(self):
+        return f"{self.pet_type} ({self.pet_business})"
