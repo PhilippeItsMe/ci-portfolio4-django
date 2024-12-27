f285fc906af6de45fd7bf9a55a9a691062f421d5
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-12-27 10:46:12 +0000

    Heroku setting

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-^wlq=w-gaf$)-it&c_qps+!$c1dumq*mpj=0d%9k^%b5+$_0ty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','8000-philippeits-ciportfolio-bjahf5p06ax.ws-eu117.gitpod.io']
+ALLOWED_HOSTS = ['localhost','8000-philippeits-ciportfolio-bjahf5p06ax.ws-eu117.gitpod.io','.herokuapp.com']
 
 
 # Application definition
