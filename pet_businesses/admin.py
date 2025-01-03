4840dd7e76df56f49decff17b01225c78481b8f0
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-01-03 21:16:01 +0000

    Add post to comment

--- pet_businesses/admin.py
+++ pet_businesses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pet_Businesse, Service_Type, Pet_Type
+from .models import Pet_Businesse, Service_Type, Pet_Type, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -8,19 +8,23 @@ class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('firm', 'locality','approved')
     search_fields = ['firm', 'locality']
-    # list_filter = ('business_pet_type')
     prepopulated_fields = {'slug': ('firm',)}
     summernote_fields = ('description',)
 
 @admin.register(Service_Type)
 class PostAdmin(SummernoteModelAdmin):
     
-    list_display = ('service_type',)
+    list_display = ('service_type', 'date_created')
    
 @admin.register(Pet_Type)
 class PostAdmin(SummernoteModelAdmin):
     
-    list_display = ('pet_type',)
+    list_display = ('pet_type','date_created')
+
+@admin.register(Comment)
+class PostAdmin(SummernoteModelAdmin):
+    
+    list_display = ('pet_businesse', 'author', 'approved','date_created') 
 
 # Register your models here.
 # admin.site.register(Service_Type)
